50d7491e8c2a5289ec630a96c5abc920675773a6
Paul Thibodeaux <paul.thibodeaux@towerswatson.com>
2019-12-01 13:00:57 -0700

    refactor

--- ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -12,28 +12,16 @@ export class FetchDataComponent implements OnInit {
   public forecasts: WeatherForecast[];
   public settings: RuntimeSetting[];
 
-  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string, private snapshotService: SnapshotService) {
+  constructor(private http: HttpClient,  @Inject('BASE_URL') private baseUrl: string, private snapshotService: SnapshotService) {
     // alert(baseUrl + 'weatherforecast');
-
-    // try {
-    //   http.get<ResultEnvelope>('http://qa.dstransformation.com/snapshot/api/SnapshotControls/GetRuntimeSettings').subscribe(result => {
-    //     this.settings = result.resultContent as RuntimeSetting[];
-    //     console.log('@@@@ ' + JSON.stringify(this.settings[0]));
-    //     //alert(this.settings[0].settingname);
-    //     //this.forecasts = result;
-    //   }, error => console.error(error));
-  
-    // } catch (error) {
-      
-    // }
-
-    http.get<WeatherForecast[]>(baseUrl + 'weatherforecast').subscribe(result => {
-      this.forecasts = result;
-    }, error => console.error(error));
   }
 
   ngOnInit(): void {
+    //this.getSettings();
     this.getSettingsViaObservable();
+    this.http.get<WeatherForecast[]>(this.baseUrl + 'weatherforecast').subscribe(result => {
+      this.forecasts = result;
+    }, error => console.error(error));
   }
 
   getSettingsViaObservable(): void {
@@ -44,6 +32,18 @@ export class FetchDataComponent implements OnInit {
             console.log(JSON.stringify(this.settings));
           }
           );
+  }
+  getSettings(): void {
+    try {
+      this.http.get<ResultEnvelope>('http://qa.dstransformation.com/snapshot/api/SnapshotControls/GetRuntimeSettings').subscribe(result => {
+        this.settings = result.resultContent as RuntimeSetting[];
+        console.log('@@@@ ' + JSON.stringify(this.settings[0]));
+        //alert(this.settings[0].settingname);
+        //this.forecasts = result;
+      }, error => console.error(error));
+  
+    } catch (error) {
+    }
   }
 }
 
